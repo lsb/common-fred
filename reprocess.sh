8f76e9638a14503974100ac8dd4b63b2eada9cd1
Lee Butterman <leebutterman@gmail.com>
2022-01-20 22:26:33 -0800

    less reduplication, less disk usage

--- reprocess.sh
+++ reprocess.sh
@@ -1,2 +1,3 @@
-(cat specialrequests.tsv; tail -n +2 sentences.tsv | sort -u) | parallel --pipe -j 1 -N 1 'tee txt/{#}.txt | say -v Fred -o wave/{#}.wave'
-(cat specialrequests.tsv; tail -n +2 sentences.tsv | sort -u) | parallel --eta -N 1 'opusenc --quiet --bitrate 16 --speech wave/{#}.wave opus/{#}.opus'
+mkdir -p txt wave opus
+(cat specialrequests.tsv; tail -n +2 sentences.tsv | sort -u) | parallel --pipe -j 1 -N 1 'cat > txt/{#}.txt'
+ls txt/ | parallel --eta -j 1 -N 1 'say -v Fred -o wave/{/.}.wave < txt/{/.}.txt ; opusenc --quiet --bitrate 16 --speech wave/{/.}.wave opus/{/.}.opus ; rm wave/{/.}.wave'
